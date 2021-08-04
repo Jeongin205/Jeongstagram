e2d906deb652d9f333b783f8b151faa70e45d79f
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-08-04 12:30:02 +0900

    시작화면

--- app/src/main/java/com/example/jeongstagram/StartActivity.java
+++ app/src/main/java/com/example/jeongstagram/StartActivity.java
@@ -2,7 +2,9 @@ package com.example.jeongstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
     }
 }
