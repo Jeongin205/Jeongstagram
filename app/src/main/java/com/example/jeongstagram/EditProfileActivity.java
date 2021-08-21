ab9646f0e17f072686873be85f746859d2fe606b
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-08-22 01:24:56 +0900

    프로필 편집창

--- app/src/main/java/com/example/jeongstagram/EditProfileActivity.java
+++ app/src/main/java/com/example/jeongstagram/EditProfileActivity.java
@@ -6,10 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.jeongstagram.databinding.ActivityEditProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,12 +24,17 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class EditProfileActivity extends AppCompatActivity {
     ActivityEditProfileBinding binding;
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     StorageReference storageReference = FirebaseStorage.getInstance().getReference();
     String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    Uri selectedImageUri;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("User").child(uid);
+    boolean isName = false;
+    private final int GET_GALLERY_IMAGE = 200;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +44,7 @@ public class EditProfileActivity extends AppCompatActivity {
         binding.btnCancel.setOnClickListener(v -> {
             finish();
         });
-        databaseReference.child("User").child(uid).addValueEventListener(new ValueEventListener() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 UserAccount account = snapshot.getValue(UserAccount.class);
@@ -50,9 +61,65 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
+        binding.profile.setOnClickListener(v -> {
+            setImage();
+        });
+        binding.tvProfile.setOnClickListener(v -> {
+            setImage();
+        });
+
+        binding.etName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(binding.etName.getText().toString().replace(" ", "").equals("")){
+                    binding.tvErrorName.setVisibility(View.VISIBLE);
+                    isName = false;
+                }
+                else{
+                    binding.tvErrorName.setVisibility(View.INVISIBLE);
+                    isName = true;
+                }
+            }
+        });
+
+        binding.btnEdit.setOnClickListener(v -> {
+            if(isName){
+                if (selectedImageUri != null) {
+                    FirebaseStorage.getInstance().getReference().child("userImages").child(uid).putFile(selectedImageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {}
+                    });
+                }
+                String name = binding.etName.getText().toString();
+                String introduce = binding.etIntroduce.getText().toString();
+                databaseReference.child("name").setValue(name);
+                databaseReference.child("introduce").setValue(introduce);
+                finish();
+            }
+            else Toast.makeText(getApplicationContext(), "이름을 넣어주세요", Toast.LENGTH_SHORT).show();
+        });
+
         binding.userSetting.setOnClickListener(v -> {
             Intent intent = new Intent(EditProfileActivity.this, SettingActivity.class);
             startActivity(intent);
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImageUri = data.getData();
+            Glide.with(getApplicationContext()).load(selectedImageUri).into(binding.profile);
+        }
+    }
+    private void setImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, GET_GALLERY_IMAGE);
     }
 }
