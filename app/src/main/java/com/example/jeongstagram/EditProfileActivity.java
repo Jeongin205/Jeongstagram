e66f16e5921fdbfebca6dcafa8b36bee6b412aba
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-08-22 00:51:27 +0900

    프로필 편집창 기본틀

--- app/src/main/java/com/example/jeongstagram/EditProfileActivity.java
+++ app/src/main/java/com/example/jeongstagram/EditProfileActivity.java
@@ -1,13 +1,29 @@
 package com.example.jeongstagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.example.jeongstagram.databinding.ActivityEditProfileBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class EditProfileActivity extends AppCompatActivity {
     ActivityEditProfileBinding binding;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +32,27 @@ public class EditProfileActivity extends AppCompatActivity {
 
         binding.btnCancel.setOnClickListener(v -> {
             finish();
+        });
+        databaseReference.child("User").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserAccount account = snapshot.getValue(UserAccount.class);
+                binding.etName.setText(account.getName());
+                binding.etIntroduce.setText(account.getIntroduce());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+        storageReference.child("userImage/"+uid).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(EditProfileActivity.this).load(uri).into(binding.profile);
+            }
+        });
+
+        binding.userSetting.setOnClickListener(v -> {
+            Intent intent = new Intent(EditProfileActivity.this, SettingActivity.class);
+            startActivity(intent);
         });
     }
 }
